5ace3c1956da7a4095c67609dabc0cc3ff4aff81
Ary Vidot <aryvid@gmail.com>
2020-06-04 21:06:24 +0100

    New sequence flashing button added

--- game.js
+++ game.js
@@ -1,11 +1,29 @@
-//  Detecting mouse click
+var gamePattern = [];
 
-$("btn").click(function (event) {
-  $("btn").addClass("pressed");
-});
+// Button Colour
+var buttonColours = ["red", "blue", "green", "yellow"];
 
 // New Sequence
 function nextSequence() {
-  var randomNumber = Math.random(n * 3);
-  console.log(randomNumber);
+  var randomNumber = Math.floor(Math.random() * 4); // 1-4
+  var randomChosenColour = buttonColours[randomNumber];
+
+  // console.log(randomChosenColour);
+  gamePattern.push(randomChosenColour);
+
+  // Select button with same id + flashing
+  $("#" + randomChosenColour)
+    .fadeOut(100)
+    .fadeIn(100);
 }
+
+nextSequence();
+console.log(gamePattern);
+
+var sound = new Audio("sounds/" + nextSequence + ".mp3");
+sound.play();
+
+//  Detecting mouse click
+// $("btn").click(function (event) {
+//   $("btn").addClass("pressed");
+// });
